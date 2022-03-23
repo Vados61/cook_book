adb8c47bc3592be83b4c265f69376a46af1d0530
Вадим Прокофьев <pvild@yandex.com>
2022-03-23 19:37:55 +0300

    finished function get shop list

--- cook.py
+++ cook.py
@@ -6,8 +6,21 @@ def adding_dishes(quantity):
 			cook_book[name] = []
 			for i in range(int(file.readline())):
 				dish = file.readline().split('|')
-				cook_book[name].append({'ingredient_name' : dish[0], 'quantity' : int(dish[1]), 'measure' : dish[2].strip()})
+				cook_book[name].append({'ingredient_name' : dish[0].strip(), 'quantity' : int(dish[1]), 'measure' : dish[2].strip()})
 			file.readline()
-	return print(cook_book)
+	return cook_book
 
-adding_dishes(4)
+def get_shop_list_by_dishes(dishes, person_count):
+	cook_book = adding_dishes(4)
+	products  = {}
+	for dish in dishes:
+		for product in cook_book[dish]:
+			product['quantity'] *= person_count
+			name = product.pop('ingredient_name')
+			if name not in products.keys():
+				products[name] = product
+			else:
+				products[name]['quantity'] += product['quantity']
+	return print(products)
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Фахитос', 'Омлет'], 2)
